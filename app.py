02b88c01550b985974e6042a41077f3289ad6898
kulyash22 <48837035+kulyash22@users.noreply.github.com>
2021-03-01 17:50:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 #WELCOME TO KULCHAU'S FLASK APP
+
+
 from flask import Flask, render_template, request, redirect
 from PIL import Image as im
 import pytesseract
@@ -18,9 +20,89 @@ def find_score(arr, angle):
   return hist, score
 
 
+def aadhaar_double_check(extractedInformation):
+  n = len(extractedInformation)
+  to_check1 = "DOB : **/**/"
+  to_check2 = "Year of Birth : "
+  max_score = len(to_check1) * len(to_check2)
+  max_match1 = 0
+  max_match2 = 0
+  for i in range(n - len(to_check1)):
+    curr_match1 = 0
+    for j in range(len(to_check1)):
+      if to_check1[j] == '*' or to_check1[j] == extractedInformation[i + j]:
+        curr_match1 += 1
+    max_match1 = max(max_match1, curr_match1)
+  for i in range(n - len(to_check2)):
+    curr_match2 = 0
+    for j in range(len(to_check2)):
+      if to_check2[j] == '*' or to_check2[j] == extractedInformation[i + j]:
+        curr_match2 += 1
+    max_match2=max(max_match2, curr_match2)
+  max_match1 = max_match1 * len(to_check2)
+  max_match2 = max_match2 * len(to_check1)
+  final_score = max(max_match1, max_match2) * 100 / max_score
+  return str(final_score)
+
+
+def passport_double_check(extractedInformation):
+  n = len(extractedInformation)
+  to_check1 = "REPUBLIC OF INDIA"
+  to_check2 = "Place of Issue"
+  max_score = len(to_check1) * len(to_check2)
+  max_match1 = 0
+  max_match2 = 0
+  for i in range(n - len(to_check1)):
+    curr_match1 = 0
+    for j in range(len(to_check1)):
+      if to_check1[j] == extractedInformation[i + j]:
+        curr_match1 += 1
+    max_match1 = max(max_match1, curr_match1)
+  for i in range(n - len(to_check2)):
+    curr_match2 = 0
+    for j in range(len(to_check2)):
+      if to_check2[j] == extractedInformation[i + j]:
+        curr_match2 += 1
+    max_match2 = max(max_match2, curr_match2)
+  max_match1 = max_match1 * len(to_check2)
+  max_match2 = max_match2 * len(to_check1)
+  final_score = max(max_match1, max_match2) * 100 / max_score
+  return str(final_score)
+
+
+def pan_double_check(extractedInformation):
+  n = len(extractedInformation)
+  to_check1 = "income tax department"
+  to_check2 = "permanent account number"
+  max_score = len(to_check1) * len(to_check2)
+  max_match1 = 0
+  max_match2 = 0
+  for i in range(n - len(to_check1)):
+    curr_match1 = 0
+    for j in range(len(to_check1)):
+      c = extractedInformation[i + j];
+      if c >= 'A' and c <= 'Z':
+        c = c.lower()
+      if to_check1[j] == c:
+        curr_match1 += 1
+    max_match1 = max(max_match1, curr_match1)
+  for i in range(n - len(to_check2)):
+    curr_match2 = 0
+    for j in range(len(to_check2)):
+      c = extractedInformation[i + j];
+      if c >= 'A' and c <= 'Z':
+        c = c.lower()
+      if to_check2[j] == c:
+        curr_match2 += 1
+    max_match2 = max(max_match2, curr_match2)
+  max_match1 = max_match1 * len(to_check2)
+  max_match2 = max_match2 * len(to_check1)
+  final_score = max(max_match1, max_match2) * 100 / max_score
+  return str(final_score)
+
+
 def main(img):
-  # pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
-  img_raw=img
+  img_raw = img
   #SKEW CORRECTION ON IMG
   wd, ht = img.size
   delta = 1
@@ -42,7 +124,8 @@ def main(img):
   img1 = np.array(img)
   img1 = img1[:, :, ::-1].copy()
   img1_raw = img1
-  img1 = cv2.medianBlur(img1,5)
+  img1 = cv2.medianBlur(img1_raw,5)
+
 
   extractedInformation1 = pytesseract.image_to_string(img1)
   #IMG1 AND IMG1_RAW ARE THE SKEW CORRECTED IMAGES
@@ -51,7 +134,7 @@ def main(img):
   img2 = np.array(img_raw)
   img2 = img2[:,:,::-1].copy()
   img2_raw = img2
-  img2 = cv2.medianBlur(img2,5)
+  img2 = cv2.medianBlur(img2_raw,5)
   extractedInformation6 = pytesseract.image_to_string(img2)
   #IMG2 AND IMG2_RAW ARE THE NON - SKEW CORRECTED IMAGES
 
@@ -75,69 +158,11 @@ def main(img):
 
 
   #CONCATENATION OF ALL THE OCR STRINGS 
-  extractedInformation  = str(extractedInformation1 + " " + extractedInformation2 + " " + extractedInformation3 + " " + extractedInformation4 + " " + extractedInformation5 + " " + extractedInformation6)
-  n=len(extractedInformation)
+  extractedInformation  = "   " + str(extractedInformation1 + " " + extractedInformation2 + " " + extractedInformation3 + " " + extractedInformation4 + " " + extractedInformation5 + " " + extractedInformation6) + "   "
+  n = len(extractedInformation)
 
 
   #CODE FOR SEARCHING THE FINAL STRING FOR THE UIDs SPECIFIC TO ALL TYPES OF KYCs
-
-  identiciation = ""
-  #VOTER ID CHECK
-  vote=0
-  startvo=-1
-  for i in range(n-10):
-    c=0
-    for j in range(10):
-      if j<=2 and extractedInformation[i+j]>='A' and extractedInformation[i+j]<='Z':
-        c+=1
-      elif j>2 and extractedInformation[i+j]>='0' and extractedInformation[i+j]<='9':
-        c+=1
-    if i>0 and (extractedInformation[i-1]==' ' or extractedInformation[i-1]=='\n'):
-      c+=1
-    elif i==0:
-      c+=1
-    if i+10<n and extractedInformation[i+10]==' ' or extractedInformation[i+10]=='\n':
-      c+=1
-    elif i==n-10:
-      c+=1
-    if c==12:
-      vote=1
-      startvo=i
-      break
-  if vote==1:
-    retval=str(extractedInformation[startvo:startvo+10])
-    identiciation = "Voter ID"
-    return retval, identification
-
-
-  #PAN CARD CHECK
-  pan=0
-  startpan=-1
-  for i in range(n-10):
-    c=0
-    for j in range(10):
-      if j<=4 and extractedInformation[i+j]>='A' and extractedInformation[i+j]<='Z':
-        c+=1
-      elif j==9 and extractedInformation[i+j]>='A' and extractedInformation[i+j]<='Z':
-        c+=1
-      elif j>4 and j<9 and extractedInformation[i+j]>='0' and extractedInformation[i+j]<='9':
-        c+=1
-    if i>0 and (extractedInformation[i-1]==' ' or extractedInformation[i-1]=='\n'):
-      c+=1
-    elif i==0:
-      c+=1
-    if i+10<n and extractedInformation[i+10]==' ' or extractedInformation[i+10]=='\n':
-      c+=1
-    elif i==n-10:
-      c+=1
-    if c==12:
-      pan=1
-      startpan=i
-      break
-  if pan==1:
-    retval=str(extractedInformation[startpan:startpan+10])
-    identiciation = "PAN Card"
-    return retval, identiciation
 
 
   #AADHAAR CARD CHECK
@@ -165,37 +190,42 @@ def main(img):
       startaa=i
       break
   if aadhaar==1:
-    retval=str(extractedInformation[startaa:startaa+14])
-    identiciation = "Aadhaar Card"
-    return retval, identiciation
+    uid=str(extractedInformation[startaa:startaa+14])
+    kyc_type="Aadhaar Card"
+    final_score=aadhaar_double_check(extractedInformation)
+    print(final_score)
+    return uid, kyc_type,final_score
+ 
 
-
-  #PASSPORT CARD CHECK  
-  passport=0
-  startpa=-1
-  for i in range(n-8):
+  #PAN CARD CHECK
+  pan=0
+  startpan=-1
+  for i in range(n-10):
     c=0
-    for j in range(8):
-      if j==0 and extractedInformation[i+j]>='A' and extractedInformation[i+j]<='Z':
+    for j in range(10):
+      if j<=4 and extractedInformation[i+j]>='A' and extractedInformation[i+j]<='Z':
         c+=1
-      elif j>0 and extractedInformation[i+j]>='0' and extractedInformation[i+j]<='9':
+      elif j==9 and extractedInformation[i+j]>='A' and extractedInformation[i+j]<='Z':
+        c+=1
+      elif j>4 and j<9 and extractedInformation[i+j]>='0' and extractedInformation[i+j]<='9':
         c+=1
     if i>0 and (extractedInformation[i-1]==' ' or extractedInformation[i-1]=='\n'):
       c+=1
     elif i==0:
       c+=1
-    if i+8<n and extractedInformation[i+8]==' ' or extractedInformation[i+8]=='\n':
+    if i+10<n and extractedInformation[i+10]==' ' or extractedInformation[i+10]=='\n':
       c+=1
-    elif i==n-8:
+    elif i==n-10:
       c+=1
-    if c==10:
-      passport=1
-      startpa=i
+    if c==12:
+      pan=1
+      startpan=i
       break
-  if passport==1:
-    retval=str(extractedInformation[startpa:startpa+8])
-    identiciation = "Passport Card"
-    return retval, identiciation
+  if pan==1:
+    uid=str(extractedInformation[startpan:startpan+10])
+    kyc_type="PAN Card"
+    final_score=pan_double_check(extractedInformation)
+    return uid, kyc_type,final_score
 
 
   #DRIVING LICENCE CHECK
@@ -223,9 +253,68 @@ def main(img):
       startli=i
       break
   if licence==1:
-    retval=str(extractedInformation[startli:startli+16])
-    identiciation = "Driving Licence Card"
-    return retval, identiciation
+    uid=str(extractedInformation[startli:startli+16])
+    kyc_type="Driving Licence"
+    final_score=aadhaar_double_check(extractedInformation)
+    return uid, kyc_type,final_score
+
+
+  #PASSPORT CARD CHECK  
+  passport=0
+  startpa=-1
+  for i in range(n-8):
+    c=0
+    for j in range(8):
+      if j==0 and extractedInformation[i+j]>='A' and extractedInformation[i+j]<='Z':
+        c+=1
+      elif j>0 and extractedInformation[i+j]>='0' and extractedInformation[i+j]<='9':
+        c+=1
+    if i>0 and (extractedInformation[i-1]==' ' or extractedInformation[i-1]=='\n'):
+      c+=1
+    elif i==0:
+      c+=1
+    if i+8<n and extractedInformation[i+8]==' ' or extractedInformation[i+8]=='\n':
+      c+=1
+    elif i==n-8:
+      c+=1
+    if c==10:
+      passport=1
+      startpa=i
+      break
+  if passport==1:
+    uid=str(extractedInformation[startpa:startpa+8])
+    kyc_type="Passport"
+    final_score=passport_double_check(extractedInformation)
+    return uid, kyc_type,final_score
+
+
+  #VOTER ID CHECK
+  vote=0
+  startvo=-1
+  for i in range(n-10):
+    c=0
+    for j in range(10):
+      if j<=2 and extractedInformation[i+j]>='A' and extractedInformation[i+j]<='Z':
+        c+=1
+      elif j>2 and extractedInformation[i+j]>='0' and extractedInformation[i+j]<='9':
+        c+=1
+    if i>0 and (extractedInformation[i-1]==' ' or extractedInformation[i-1]=='\n'):
+      c+=1
+    elif i==0:
+      c+=1
+    if i+10<n and extractedInformation[i+10]==' ' or extractedInformation[i+10]=='\n':
+      c+=1
+    elif i==n-10:
+      c+=1
+    if c==12:
+      vote=1
+      startvo=i
+      break
+  if vote==1:
+    uid=str(extractedInformation[startvo:startvo+10])
+    kyc_type="Voter's Identity Card"
+    final_score=aadhaar_double_check(extractedInformation)
+    return uid, kyc_type,final_score
 
 
   #JOB CARD CHECK
@@ -261,45 +350,59 @@ def main(img):
       startnr=i
       break
   if nrega==1:
-    retval=str(extractedInformation[startnr:startnr+21])
-    identiciation = "NREGA Card"
-    return retval, identiciation
+    uid=str(extractedInformation[startnr:startnr+21])
+    kyc_type="NREGA Card"
+    final_score=aadhaar_double_check(extractedInformation)
+    return uid, kyc_type,final_score
 
 
-  #IF NOTHING IS FOUNG 
-  retval="Please try again, the image was not clear enough"
-  identiciation = "none found"
-  return retval, identiciation
+  #IF NOTHING IS FOUND
+  uid="Please try again, the image was not clear enough"
+  kyc_type= "none found"
+  return uid, kyc_type, "---"
 
 
-@app.route('/',methods=['POST','GET'])
+@app.route('/',methods = ['POST', 'GET'])
 def kulchau():
-    if request.method == 'POST':
-        file = request.files['image'].read()
-        bg = im.open(io.BytesIO(file))
-        answer, uid = main(bg)
-        return redirect('/{}-{}'.format(answer,uid))
-    return render_template('index.html')
+  if request.method == 'POST':
+    file = request.files['image'].read()
+    bg = im.open(io.BytesIO(file))
+    uid, kyc_type,final_score = main(bg)
+    return redirect('/{}-{}-{}'.format(uid, kyc_type, final_score))
+  return render_template('index.html')
+ 
 
-# @app.route('/server',methods=['POST','GET'])
-# def forserver():
+@app.route('/server',methods=['POST','GET'])
+def forserver():
+  pass
 
 
-@app.route('/<result>',methods=['POST','GET'])
+@app.route('/<result>',methods=['POST', 'GET'])
 def new_page(result):
-    uid = ""
-    answer = ""
-    j = -1
-    for i in range(len(result)):
-        if(result[i]=='-'):
-            j = i
-            break
-        else:
-            uid = uid + result[i]
-    for i in range(j+1,len(result)):
-        answer = answer + result[i]
+  uid = ""
+  kyc_type = ""
+  final_score = ""
+  j = -1
+  for i in range(len(result)):
+    if(result[i] == '-'):
+      j = i
+      break
+    else:
+      uid = uid + result[i]
+  for i in range(j + 1, len(result)):
+    if(result[i] == '-'):
+      j = i
+      break
+    else:
+      kyc_type = kyc_type + result[i]
 
-    return render_template('result.html',identif=answer,uiid=uid)
+  for i in range(j + 1, len(result)):
+    final_score = final_score + result[i]
+
+  flag=True
+  if kyc_type == 'none found':
+    flag = False
+  return render_template('result.html', identif = kyc_type, uiid = uid, flag = flag, final_score = final_score)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(host = '0.0.0.0', port = 5000, debug = True)
